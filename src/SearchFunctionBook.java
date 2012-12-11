31444afe20c013a69e71abe04479791ae61ff03f
mattgenious <matt.l.p.jensen@gmail.com>
2012-12-11 13:13:36 +0100

    Returns list of dates based on fromcity and tocity

---
+++ src/SearchFunctionBook.java
@@ -0,0 +1,31 @@
+import java.awt.List;
+import java.sql.ResultSet;
+
+public class SearchFunctionBook
+{	
+	static List st = new List();
+	
+	// Searching the database.
+	// Takes DataBase.getEntry(string firstName, string lastName, string BirthDate, string ZipCode, string PhoneNumber,
+	// string BookingNumber): empty strings allowed.
+		public List getEntry(String qr1, String qr2) throws Exception
+		{
+			String query =	"SELECT DepartureDate," +
+							" FlightID FROM Flights" + " WHERE FromCity LIKE '" + qr1 + "' AND ToCity LIKE '" + qr2 +
+							"' ORDER BY DepartureDate";
+
+			ResultSet rs = Database.getInstance().execute(query);
+		
+				while (rs.next())
+				{
+					for ( int i = 1; i < rs.getMetaData().getColumnCount(); i++)
+						st.add(rs.getMetaData().getColumnName(i)+": "+rs.getString(rs.getMetaData().getColumnName(i)));
+				}
+				
+//			for (int i = 0; i < st.getItemCount(); i++) {
+//				System.out.println(st.getItem(i));
+//			}
+				
+			return st;
+		}
+}
