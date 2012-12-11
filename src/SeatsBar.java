97b759ec1876c4f0527a659be7e4349fea2f0080
Emil Dalsbaek <emil.dalsbaek@gmail.com>
2012-12-11 15:13:19 +0100

    asd

---
+++ src/SeatsBar.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SeatsBar implements GUIComponent
+{
+	JFrame frame;
+	
+	public void makeFrame()
+	{
+		// Sets up the frame and its dimensions.
+			Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+			Dimension size = new Dimension((int)(d.width*(1136f/1920f)), (int)(d.height*(350f/1080f)));
+			
+			frame = new JFrame();
+				frame.setSize(size.width, size.height);
+				frame.setUndecorated(false);
+				frame.setResizable(true);
+				frame.getContentPane().setBackground(Color.getHSBColor(0.55f, 0.55f, 0.75f));
+				frame.setLocation((int)(d.width*(392f/1920f)), (int)(d.height*(77f/1080f)));
+				frame.setLayout(new BorderLayout());
+				
+			JLabel nose = new JLabel(new ImageIcon("nose.PNG"));
+			JLabel tail = new JLabel(new ImageIcon("tail.PNG"));
+			JLabel topwing = new JLabel(new ImageIcon("topwing.PNG"));
+			JLabel bottomwing = new JLabel(new ImageIcon("botomwing"));
+			JLabel body = new JLabel(new ImageIcon("body"));
+			
+			frame.add(nose, BorderLayout.WEST);
+			frame.add(tail, BorderLayout.EAST);
+			frame.add(topwing, BorderLayout.NORTH);
+			frame.add(bottomwing, BorderLayout.SOUTH);
+	}
+
+	public void makeButtons()
+	{
+			
+	}
+
+	public void showFrame()
+	{
+		frame.setVisible(true);
+	}
+}
