09bbf734f378793c62c0b00084463073abb97759
mattgenious <matt.l.p.jensen@gmail.com>
2012-12-11 12:34:17 +0100

    SearchFunctionFlight

---
+++ src/SearchFunctionFlights.java
@@ -0,0 +1,33 @@
+import java.awt.List;
+import java.sql.ResultSet;
+
+public class SearchFunctionFlights
+{	
+	static List st = new List();
+	
+	// Searching the database.
+	// Takes DataBase.getEntry(string firstName, string lastName, string BirthDate, string ZipCode, string PhoneNumber,
+	// string BookingNumber): empty strings allowed.
+		public List getEntry(String qr1, String qr2) throws Exception
+		{
+			String query =	"SELECT FlightID, FromCity, ToCity, DepartureDate, SeatsTotal," +
+							" FlightID FROM Flights" + " WHERE FromCity LIKE '" + qr1 + "' AND ToCity LIKE '" + qr2 +
+							"' ORDER BY FlightID";
+
+			ResultSet rs = Database.getInstance().execute(query);
+		
+				while (rs.next())
+				{
+					for ( int i = 1; i < rs.getMetaData().getColumnCount(); i++)
+						st.add(rs.getMetaData().getColumnName(i)+": "+rs.getString(rs.getMetaData().getColumnName(i)));
+					
+					st.add("");
+				}
+				
+//			for (int i = 0; i < st.getItemCount(); i++) {
+//				System.out.println(st.getItem(i));
+//			}
+				
+			return st;
+		}
+}
