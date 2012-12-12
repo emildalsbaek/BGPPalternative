1560351384c564a206f2f344039b4d39926e8d90
Matt Luccas Phaure Jensen <matt.l.p.jensen@gmail.com>
2012-12-12 17:57:27 +0100

    added method to SearchFunctionFlights to get SeatsTotal

--- src/SearchFunctionFlights.java
+++ src/SearchFunctionFlights.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 public class SearchFunctionFlights
 {	
 	static List st = new List();
+	static int SeatsTotal = 0;
 	
 	// Searching the database.
 	// Takes DataBase.getEntry(string fromcity, string tocity)
@@ -29,5 +30,21 @@ public class SearchFunctionFlights
 //			}
 				
 			return st;
+		}
+		
+		public static int getMaxSeats(int qr1) throws Exception
+		{
+			String query =	"SELECT SeatsTotal," +
+							" FlightID FROM Flights" + " WHERE FlightID = '" + qr1 + "'" +
+							"'";
+
+			ResultSet rs = Database.getInstance().execute(query);
+		
+				while (rs.next())
+				{
+					for ( int i = 1; i < rs.getMetaData().getColumnCount(); i++)
+						SeatsTotal = rs.getInt(i);
+				}
+			return SeatsTotal;
 		}
 }
