e7494bf7841cce120f883fef183d4272aaac4cb5
mattgenious <matt.l.p.jensen@gmail.com>
2012-12-13 12:59:19 +0100

    tissemand/dillergøj

--- src/SearchFunctionSeat.java
+++ src/SearchFunctionSeat.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class SearchFunctionSeat
 {	
 	static ArrayList<String> st = new ArrayList<String>();
+	static ArrayList<String> st2 = new ArrayList<String>();
 	
 	// Searching the database.
 	// Takes DataBase.getEntry(int FlightID)
@@ -24,5 +25,26 @@ public class SearchFunctionSeat
 					}
 				}	
 			return st;
+		}
+		
+	// Searching the database.
+	// Takes DataBase.getEntry(int ID)
+	// empty strings allowed.
+		public static ArrayList<String> getReservedSeats(int qr1) throws Exception
+		{
+			String query =	"SELECT Seats," +
+							" ID FROM passengers" + " WHERE ID = '" + qr1 + "'";
+			
+				ResultSet rs = Database.getInstance().execute(query);
+				while (rs.next())
+				{
+					for ( int i = 1; i < rs.getMetaData().getColumnCount(); i++)
+					{
+					String[] str = rs.getString(i).split(",");
+						for( int k = 0; k < str.length; k++ )
+							st2.add(str[k]);
+					}
+				}	
+			return st2;
 		}
 }
